519f639e34811132114bb572ff28179b326c11e2
BriPec <91296802+BriPec@users.noreply.github.com>
2022-01-19 15:38:26 +0800

    Code 1, index.html deleted

--- SourceCode/Code1/Program.cs
+++
@@ -1,9 +0,0 @@
-class Program
-{
-    public static void Main(string[] args)
-    {
-        System.Console.WriteLine("Hello Bree, Jen, and JP");
-        System.Console.WriteLine("Hello!");
-
-    }
-}
