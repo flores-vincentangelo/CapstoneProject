e5c7b5f2dd06e16c5832f76eee77ef1dcc317088
BriPec <91296802+BriPec@users.noreply.github.com>
2022-01-19 14:59:24 +0800

    Update program.cs
    Update program.cs

--- SourceCode/Code1/Program.cs
+++ SourceCode/Code1/Program.cs
@@ -3,5 +3,7 @@ class Program
     public static void Main(string[] args)
     {
         System.Console.WriteLine("Hello Bree, Jen, and JP");
+        System.Console.WriteLine("Hello!");
+
     }
 }
