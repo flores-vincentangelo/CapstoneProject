e62edd3d468e62de14e67551dc6b43b0e0bb9d54
BriPec <91296802+BriPec@users.noreply.github.com>
2022-01-19 18:21:09 +0800

    Login Page

--- SourceCode/Website/Pastebook/Controllers/HomeController.cs
+++ SourceCode/Website/Pastebook/Controllers/HomeController.cs
@@ -3,10 +3,5 @@ using Microsoft.AspNetCore.Mvc;
 
 public class HomeController: Controller
 {
-    [HttpGet]
-    [Route("/")]
-    public IActionResult GetHome()
-    {
-        return Ok("Hello World");
-    }
+    
 }
---
+++ SourceCode/Website/Pastebook/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+namespace Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+public class LoginController : Controller
+{
+    [HttpGet]
+    [Route("/")]
+    public IActionResult GetLoginAction() {
+        return View("/Views/Login.cshtml");
+    }
+}
