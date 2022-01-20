73d04192686c0bf69f57a71fa106ae08c439ea12
BriPec <91296802+BriPec@users.noreply.github.com>
2022-01-20 20:35:15 +0800

    ConfirmPassword field (Register.cshtml and js)

--- SourceCode/Website/Pastebook/wwwroot/Scripts/Register.js
+++ SourceCode/Website/Pastebook/wwwroot/Scripts/Register.js
@@ -9,3 +9,17 @@ function numbersInput(input) {
     var regex = /[^0-9]/g;
     input.value = input.value.replace(regex,"");
 }
+
+//CONFIRM PASSWORD
+document.querySelector('.signup-button').onclick = function() {
+    var password = document.querySelector('.password').value;
+    var confirmPassword = document.querySelector('.confirm-password').value;
+    if (password != confirmPassword) {
+        alert("Passwords do not match. Try again.");
+        return false;
+    }
+    else if (password == confirmPassword) {
+        return true;
+    }
+    return true;
+}
