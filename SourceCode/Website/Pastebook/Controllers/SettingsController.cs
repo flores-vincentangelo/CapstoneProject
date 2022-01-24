e5c5ed3114031dad14119f067313e84ab2e78eb0
BriPec <91296802+BriPec@users.noreply.github.com>
2022-01-24 13:24:37 +0800

    Settings

--- SourceCode/Website/Pastebook/Controllers/SettingsController.cs
+++ SourceCode/Website/Pastebook/Controllers/SettingsController.cs
@@ -6,63 +6,19 @@ using Models;
 public class SettingsController : Controller
 {
     [HttpGet]
-    [Route("/settings")]
-    public IActionResult GetSettingsAction() {
-        return View("/Views/Settings/Settings.cshtml");
+    [Route("/settings/{id}")]
+    public IActionResult GetInfoById(string id) {
+        var user = DbUsers.GetInformationById(id);
+        return View("/Views/Settings/Settings.cshtml", user);
     }
-    
-    [HttpPost]
-    [Route("/register")]
-    public IActionResult doPostRegistration()
-    {
-        var firstName = HttpContext.Request.Form["FirstName"];
-        var lastName = HttpContext.Request.Form["LastName"];
-        var emailAddress = HttpContext.Request.Form["EmailAddress"];
-        var mobileNumber = HttpContext.Request.Form["MobileNumber"];
-        var password = HttpContext.Request.Form["Password"];
-        var birthday = HttpContext.Request.Form["Birthday"];
-        DateTime birthDate = DateTime.Parse(birthday);
-        var dateOfBirth = (long)((birthDate.Subtract(new System.DateTime(1970, 1, 1, 0, 0, 0, 0))).TotalSeconds);
-        var gender = HttpContext.Request.Form["Gender"];
-        
-        var model = new UserModel();
-        var isEmailExists = DbUsers.checkEmailAddress(emailAddress);
-        if (isEmailExists)
-        {
-            return View("Views/Register/EmailExists.cshtml");
-        }
 
-        model.FirstName = firstName;
-        model.LastName = lastName;
-        model.EmailAddress = emailAddress;
-        model.MobileNumber = mobileNumber;
-        model.Password = password;
-        model.Birthday = dateOfBirth;
-        model.Gender = gender;
-        model.FullName = ((firstName + lastName).Replace(" ", "")).ToLower();
-        var duplicate = DbUsers.checkFullName(model.FullName);
-        if(duplicate == -1)
-        {
-            duplicate = 0;
-        }
-        else 
-        { 
-            duplicate += 1; 
-        }
-
-        model.Duplicate = duplicate;
-        model.ProfileLink= model.FullName + model.Duplicate;
-        DbUsers.InsertUser(model);
-        // Added by JP
-        // Add new profile upon registration of new user
-        var profile = new ProfileModel();
-        profile.Id = model.ProfileLink;
-        profile.FullName = model.FirstName + " " + model.LastName;
-        profile.About = "Write something about me";
-        profile.Photo = "";
-        profile.Cover = "";
-        DbProfiles.AddProfile(profile);
-        DbUsers.SendVerificationEmail(model);
-        return View("Views/Register/RegisteredSuccess.cshtml");
+    [HttpPatch]
+    [Route("/settings/{id}")]
+    public IActionResult ModifyInformation(string id,  [FromBody] UserModel user) {
+        // Modify Users Table
+        DbUsers.ModifyInformation(id, user);
+        // Get Updated Users Table
+        var newUser = DbUsers.GetInformationById(id);
+        return Json(newUser);
     }
 }
