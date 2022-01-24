19fdb3e2f3278e1164ae5d6105d6a8abe227227f
jplaruscain99 <jp.laruscain.99@gmail.com>
2022-01-24 23:08:42 +0800

    Profile Updates
    Sample Profile About page

--- SourceCode/Website/Pastebook/wwwroot/Scripts/Profile.js
+++ SourceCode/Website/Pastebook/wwwroot/Scripts/Profile.js
@@ -114,6 +114,8 @@ async function modifyProfile(event, jsonData) {
     });
     if(response.status == 200) {
         alert("Profile modified successfully!");
+        var userData = await response.json();
+        localStorage.setItem('User', JSON.stringify(userData));
     }
 }
 
