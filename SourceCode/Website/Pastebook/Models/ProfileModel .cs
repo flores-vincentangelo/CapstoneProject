2a1d7c37f92aa0637412e5dcd814e4e0ccd649b7
flores-vincentangelo <vincent.flores@pointwest.com.ph>
2022-01-24 08:47:05 +0800

    Latest Stable Build
    Fixed main branch

--- SourceCode/Website/Pastebook/Models/ProfileModel .cs
+++ SourceCode/Website/Pastebook/Models/ProfileModel .cs
@@ -1,16 +1,9 @@
 namespace Models;
 public class ProfileModel
 {
-<<<<<<< Updated upstream
-    public int? UserId {get;set;}   
-    public string? ProfilePicture {get;set;}
-    public string? About {get;set;}
-    public string? FriendsList {get;set;}
-=======
     public string? Id {get;set;}   
     public string? FullName {get;set;}   
     public string? About {get;set;}
     public string? Photo {get;set;}
     public string? Cover {get;set;}
->>>>>>> Stashed changes
 }
