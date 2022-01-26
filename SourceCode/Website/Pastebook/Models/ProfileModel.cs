5b5f659f960b57af867f2556ddb3ab2d52d8dfc2
jplaruscain99 <jp.laruscain.99@gmail.com>
2022-01-26 21:28:52 +0800

    Update ProfileModel.cs

--- SourceCode/Website/Pastebook/Models/ProfileModel.cs
+++ SourceCode/Website/Pastebook/Models/ProfileModel.cs
@@ -3,7 +3,10 @@ namespace Models;
 public class ProfileModel
 {
     public bool DoesUserOwnProfile {get;set;}
-    public UserModel User {get;set;}
+    public UserModel? User {get;set;}
+    public AlbumModel? Album {get;set;}
+    public PhotoModel? Photo {get;set;}
+    public FriendsModel? Friends {get;set;}
     public List<AlbumModel>? AlbumList {get;set;} 
     public List<PhotoModel>? PhotoList {get;set;} 
     public List<FriendsModel>? FriendsList {get;set;} 
