74a4b788e31c9efb45c06cd8d605766d4ea034f7
PerezJen <jenny.perez@pointwest.com.ph>
2022-01-26 21:11:05 +0800

    PostModel
    PostModel

--- SourceCode/Website/Pastebook/Models/PostModel.cs
+++ SourceCode/Website/Pastebook/Models/PostModel.cs
@@ -8,7 +8,6 @@ public class PostModel
     public string? Caption { get; set; }
     public int PhotoId {get;set;}
     public string? Photo {get;set;}
-    public string? Like {get;set;}
+    public string? Likes {get;set;}
     public string? Comment {get;set;}
-    public string? PostLink { get; set; }
 }
