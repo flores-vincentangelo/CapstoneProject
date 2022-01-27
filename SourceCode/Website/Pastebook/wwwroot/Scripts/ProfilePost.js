466be2690a7cd0cabc4ed3a8cbe26e1a51b8e5c4
PerezJen <jenny.perez@pointwest.com.ph>
2022-01-27 17:40:36 +0800

    Upload in Modal
    Upload in Modal

--- SourceCode/Website/Pastebook/wwwroot/Scripts/ProfilePost.js
+++ SourceCode/Website/Pastebook/wwwroot/Scripts/ProfilePost.js
@@ -2,6 +2,51 @@ $(document).ready(() => {
     viewAddPostModal();
     addPost();
     resetForm();
+
+
+    //When the user clicks on "Post" button,
+    $('.user-post-button').click((event) => {
+        event.preventDefault();           
+        //Save value of caption and photo to database
+        var caption=$('#caption').val();
+        var imageSrc=$('#modal-container-photo-img').attr('src');
+
+        if (!caption && imageSrc) {
+            const formData = {
+                Photo: imageSrc,
+                Caption: ""
+            }
+            var data = JSON.stringify(formData);
+            addPostToProfile(event, data);
+        }
+
+        else if (!imageSrc && caption) {
+            const formData = {
+                Photo: "",
+                Caption: caption
+            }
+            var data = JSON.stringify(formData);
+            addPostToProfile(event, data);
+        }
+
+        else if (imageSrc && caption) {
+            const formData = {
+                Photo: imageSrc,
+                Caption: caption
+            }
+            var data = JSON.stringify(formData);
+            addPostToProfile(event, data);
+        }
+
+        else {
+            alert("No post added!");   
+        }
+        
+        //close modal
+        $('.post-modal-container').css("display", "none");
+        resetForm();
+    });
+
 });
 
 function viewAddPostModal() {
@@ -17,18 +62,27 @@ function viewAddPostModal() {
     });
 }
 
-function addPost() {
-    //When the user clicks on "Post" button,
-    $('.user-post-button').click((event) => {
-        event.preventDefault();
-        //Save post.value to database
-        var formData = new FormData(document.getElementById('caption-add'));
-        var data = JSON.stringify(Object.fromEntries(formData.entries()));
-        addPostToProfile(event, data);
-        //close modal
-        $('.post-modal-container').css("display", "none");
-        resetForm();
-    })
+function addPost() { 
+
+    // When the user clicks on the "Choose file" button
+    $("#myPhotoPost").change(function() {
+        if (this.files && this.files[0] && this.files[0].type.includes("image")) { 
+            var reader = new FileReader();
+            reader.readAsDataURL(this.files[0]);
+            reader.onload = imageIsLoaded;
+        }
+        else {
+            alert("This is not valid image file");
+        }
+    });
+
+    //Show preview of uploaded photo
+    function imageIsLoaded(e) {
+        result = e.target.result;
+        $('#modal-container-photo-img').attr('src', result);
+        return result;
+    };
+
 }
 
 async function addPostToProfile(event, jsonData) {
@@ -36,13 +90,14 @@ async function addPostToProfile(event, jsonData) {
     const url = `/post`;
     const response = await fetch(url, {
         method: 'POST',
-        headers: {
+        headers: {  
             'Content-Type': 'application/json'
         },
         body: jsonData
     });
     if(response.status == 200) {
         alert("Post successfully added!");
+        $('#modal-container-photo-img').attr('src', "");
     }
 }
 
