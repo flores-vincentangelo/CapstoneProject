0882a61efe14848d2e7acefe2c13ea26045cdd51
BriPec <91296802+BriPec@users.noreply.github.com>
2022-01-28 20:24:07 +0800

    PostCard_Modify Modal

--- SourceCode/Website/Pastebook/wwwroot/Scripts/_PostCard.js
+++ SourceCode/Website/Pastebook/wwwroot/Scripts/_PostCard.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     openDeleteModal();
-    // openUpdateModal();
+    openUpdateModal();
 });
 
 function openDeleteModal() {
@@ -16,15 +16,15 @@ function openDeleteModal() {
     });
 }
 
-// function openUpdateModal() {
-//     $('.update-but').click(() => {
-//         // show edit form 
-//         $('#post-modal-container-update').css("display", "flex");
-//     });
+function openUpdateModal() {
+    $('.update-post').click(() => {
+        // show edit form 
+        $('#post-modal-container-update').css("display", "flex");
+    });
 
-//     // When the user clicks on the "x",
-//     $('#modal-container-close-update').click(() => {
-//         // Close modal
-//         $('#post-modal-container-update').css("display", "none");
-//     });
-// }
+    // When the user clicks on the "x",
+    $('#modal-container-close-update').click(() => {
+        // Close modal
+        $('#post-modal-container-update').css("display", "none");
+    });
+}
