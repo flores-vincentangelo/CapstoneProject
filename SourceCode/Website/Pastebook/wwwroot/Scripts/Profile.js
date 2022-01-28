9d2acd950d4be1ab012feb181f4fd084135f7137
flores-vincentangelo <vincent.flores@pointwest.com.ph>
2022-01-28 11:51:39 +0800

    Hidden the edit options if the user does not own the profile

--- SourceCode/Website/Pastebook/wwwroot/Scripts/Profile.js
+++ SourceCode/Website/Pastebook/wwwroot/Scripts/Profile.js
@@ -1,7 +1,20 @@
 $(document).ready( () => {
-    var jsonObj = JSON.parse(model);
-    console.log(jsonObj);
+    var modelObj = JSON.parse(model.replace(/&quot;/g,"\""));
+    console.log(modelObj.DoesUserOwnProfile);
     
+    if(!modelObj.DoesUserOwnProfile){
+        $("#photo-edit-btn").css("display", "none");
+        $("#about-edit-btn").css("display", "none");
+
+        $(".profile-edit-firstname-btn").css("display", "none");
+        $(".profile-edit-lastname-btn").css("display", "none");
+        $(".profile-edit-email-btn").css("display", "none");
+        $(".profile-edit-mobile-btn").css("display", "none");
+        $(".profile-edit-birthday-btn").css("display", "none");
+        $(".profile-edit-gender-btn").css("display", "none");
+        $("#profile-password").css("display", "none");
+        $(".profile-edit-password-btn").css("display", "none");
+    }
     // Add to cookie
     // document.cookie = 'profilelink=' + user.ProfileLink;
     // Profile picture
