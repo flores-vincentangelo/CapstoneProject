7e4a33e92394494d126cf54c2c1c0086932c2567
jplaruscain99 <jp.laruscain.99@gmail.com>
2022-01-28 10:00:38 +0800

    Update ProfileModel

--- SourceCode/Website/Pastebook/Models/ProfileModel.cs
+++ SourceCode/Website/Pastebook/Models/ProfileModel.cs
@@ -7,8 +7,10 @@ public class ProfileModel
     public AlbumModel? Album {get;set;}
     public PhotoModel? Photo {get;set;}
     public FriendsModel? Friends {get;set;}
+    public PostModel? Post {get;set;}
     public List<AlbumModel>? AlbumList {get;set;} 
     public List<PhotoModel>? PhotoList {get;set;} 
-    public List<FriendsModel>? FriendsList {get;set;} 
+    public List<FriendsModel>? FriendsList {get;set;}
+    public List<PostModel>? PostsList {get;set;}  
 
 }
