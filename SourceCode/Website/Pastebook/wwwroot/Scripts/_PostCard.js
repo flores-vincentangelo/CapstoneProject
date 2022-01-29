a569b131b8e92c4bea94dddc6323bb60438c6ce6
PerezJen <jenny.perez@pointwest.com.ph>
2022-01-29 18:28:34 +0800

    Profile Post Page
    Profile Post Page

--- SourceCode/Website/Pastebook/wwwroot/Scripts/_PostCard.js
+++ SourceCode/Website/Pastebook/wwwroot/Scripts/_PostCard.js
@@ -1,13 +1,42 @@
-$(document).ready(() => {
-    openDeleteModal();
-    openUpdateModal();
-    openCommentModal();
-});
+function modifyPost (postId, profileLink) {
+    //Show Update Modal
+        $('#post-modal-container-update').css("display", "flex");
 
-function openDeleteModal() {
-    $('.delete-post').click(() => {
-        // show edit form 
-        $('#post-modal-container-delete').css("display", "flex");
+    // When the user clicks on the "x",
+    $('#modal-container-close-update').click(() => {
+        // Close modal
+        $('#post-modal-container-update').css("display", "none");
+    });
+
+    //When the user clicks on "Update" button,
+    $('.user-button-update').click((event) => {
+        event.preventDefault();           
+        //Save value of text to database
+        var caption=$('.caption-update').val();      
+            const formData = {
+                Caption: caption
+            }
+            var data = JSON.stringify(formData);
+            addPostModification(event, data, postId, profileLink);
+        //close modal
+        $('#post-modal-container-update').css("display", "none");
+    });
+}
+
+
+function deletePost(postId, profileLink) {
+   //show Delete Modal 
+    $('#post-modal-container-delete').css("display", "flex");
+    //When the user clicks on the "Yes" button,
+    $('#yes-btn').click(() => {
+        //Deletes the entire post
+        deletePostById(postId, profileLink);
+    });
+
+    //When the user clicks on the "Cancel" button,
+    $('#cancel-btn').click(() => {
+        //show Delete Post Modal
+        $('#post-modal-container-delete').css("display", "none");
     });
 
     // When the user clicks on the "x",
@@ -17,28 +46,26 @@ function openDeleteModal() {
     });
 }
 
-function openUpdateModal() {
-    $('.update-post').click(() => {
-        // show edit form 
-        $('#post-modal-container-update').css("display", "flex");
+async function deletePostById(postId, profileLink) {
+    const response = await fetch(`/posts/${postId}`, {
+        method: 'DELETE',
     });
+    if (response.status == 200) {
+        location.replace(`/${profileLink}`)
+    }
 
-    // When the user clicks on the "x",
-    $('#modal-container-close-update').click(() => {
-        // Close modal
-        $('#post-modal-container-update').css("display", "none");
-    });
 }
 
-function openCommentModal() {
-    $('.post-button-comment').click(() => {
-        // show edit form 
-        $('#post-modal-container-comment').css("display", "flex");
+async function addPostModification(event, jsonData, postId, profileLink) {
+    event.preventDefault();
+    const response = await fetch(`/posts/${postId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: jsonData
     });
-
-    // When the user clicks on the "x",
-    $('#modal-container-close-comment').click(() => {
-        // Close modal
-        $('#post-modal-container-comment').css("display", "none");
-    });
+    if (response.status == 200) {
+        location.replace(`/${profileLink}`)
+    }
 }
