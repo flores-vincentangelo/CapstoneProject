ffa575394fcb631829b5fd1b60864a37e2abc972
PerezJen <jenny.perez@pointwest.com.ph>
2022-01-29 11:28:47 +0800

    Revert "Partial Profile Post Page"
    This reverts commit 78f55d9e14464bede0b8111f2544166c40b2aa1e.

--- SourceCode/Website/Pastebook/wwwroot/Scripts/_PostCard.js
+++ SourceCode/Website/Pastebook/wwwroot/Scripts/_PostCard.js
@@ -1,26 +1,6 @@
 $(document).ready(() => {
-    var modelObj = JSON.parse(model.replace(/&quot;/g,"\""));
-    console.log(modelObj)
     openDeleteModal();
     openUpdateModal();
-    deletePost(modelObj);
-
-    //When the user clicks on "Update" button,
-    $('.user-button-update').click((event) => {
-        event.preventDefault();           
-        //Save value of text to database
-        var caption=$('#caption-update').val();      
-            const formData = {
-                Caption: caption
-            }
-            var data = JSON.stringify(formData);
-            addPostModification(event, data, modelObj.PostId);
-        //close modal
-        $('#post-modal-container-update').css("display", "none");
-        location.reload();
-    });
-
-
 });
 
 function openDeleteModal() {
@@ -48,47 +28,3 @@ function openUpdateModal() {
         $('#post-modal-container-update').css("display", "none");
     });
 }
-
-function deletePost(modelObj) {
-    //When the user clicks on the "Yes" button,
-    $('#yes-btn').click(() => {
-        //Deletes the entire post
-        deletePostById(modelObj.PostId, modelObj.ProfileLink);
-    });
-
-    //When the user clicks on the "Cancel" button,
-    $('#cancel-btn').click(() => {
-        //show Delete Post Modal
-        $('#post-modal-container-delete').css("display", "none");
-    });
-
-    //When the user clicks on the "x" button,
-    $('#modal-container-close-delete').click(() => {
-        //show Delete Post Modal
-        $('#post-modal-container-delete').css("display", "none");
-    });
-
-}
-
-async function deletePostById(postId, profileLink) {
-    console.log(postId)
-    const response = await fetch(`/posts/${postId}`, {
-        method: 'DELETE',
-    });
-    if (response.status == 200) {
-        location.replace(`/${profileLink}`)
-    }
-
-}
-
-async function addPostModification(event, jsonData, postId) {
-    event.preventDefault();
-    const response = await fetch(`/posts/${postId}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: jsonData
-    });
-}
-
