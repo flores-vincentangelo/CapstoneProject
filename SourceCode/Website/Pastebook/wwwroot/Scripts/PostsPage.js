30b43885896a34b0d639c49ff3f3fcdf2d851aee
BriPec <91296802+BriPec@users.noreply.github.com>
2022-01-31 14:49:16 +0800

    PostPage, ProfilePage Like Modal

--- SourceCode/Website/Pastebook/wwwroot/Scripts/PostsPage.js
+++ SourceCode/Website/Pastebook/wwwroot/Scripts/PostsPage.js
@@ -6,6 +6,13 @@ $(document).ready(function () {
     deletePost(modelObj);
     modifyPhoto();
     openCommentModal();
+    openLikeModal();
+
+    // $('.post-container-right-action-close').click(() => {
+    //     var link = modelObj.profileLink;
+    //     console.log(link);
+    //     location.replace(`/${link}`);
+    // });
 
     //When the user clicks on "Update" button,
     $('.user-button-update').click((event) => {
@@ -132,6 +139,19 @@ function openCommentModal() {
     });
 }
 
+function openLikeModal() {
+    $('#post-container-right-status-likers').click(() => {
+        // show edit form 
+        $('#post-modal-container-likers').css("display", "flex");
+    });
+
+    // When the user clicks on the "x",
+    $('#modal-container-close-likers').click(() => {
+        // Close modal
+        $('#post-modal-container-likers').css("display", "none");
+    });
+}
+
 async function deletePostById(postId, profileLink) {
 
     const response = await fetch(`/posts/${postId}`, {
@@ -140,6 +160,5 @@ async function deletePostById(postId, profileLink) {
     if (response.status == 200) {
         location.replace(`/${profileLink}`)
     }
-
 }
 
