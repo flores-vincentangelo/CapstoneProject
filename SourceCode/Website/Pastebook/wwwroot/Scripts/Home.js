d806b587708b1523c4d336dae87c4d26aa6f45f6
jplaruscain99 <jp.laruscain.99@gmail.com>
2022-02-01 15:56:22 +0800

    Show more posts in scroll down

--- SourceCode/Website/Pastebook/wwwroot/Scripts/Home.js
+++ SourceCode/Website/Pastebook/wwwroot/Scripts/Home.js
@@ -17,6 +17,23 @@ $(document).ready(function () {
         $(".home-header-left-searchpanel-recent").css("display", "none");
     });
 
+    // Hide all posts
+    $('.profile-post-container-status-post').css("display", "none");
+    showPostsInHome();
     
-
 });
+
+function showPostsInHome() {
+    $('.profile-post-container-status-post').slice(0, 10).show();
+
+    // listen for scroll event and load more images if we reach the bottom of window
+    window.addEventListener('scroll', () => {
+        console.log("window.scrollY: ", window.scrollY) //scrolled from top
+        console.log("window.innerHeight: ", window.innerHeight) //visible part of screen
+        console.log(document.documentElement.scrollHeight) //visible part of screen
+        if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
+            console.log("bottom reached");
+            $('.profile-post-container-status-post:hidden').slice(0, 10).show().slideDown();
+        }
+    });
+}
