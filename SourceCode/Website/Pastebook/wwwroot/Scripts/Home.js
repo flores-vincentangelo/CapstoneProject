20e06f45e2096f177ce19df89a9baa8df9d9d4cd
jplaruscain99 <jp.laruscain.99@gmail.com>
2022-02-02 15:45:37 +0800

    Update Home.js

--- SourceCode/Website/Pastebook/wwwroot/Scripts/Home.js
+++ SourceCode/Website/Pastebook/wwwroot/Scripts/Home.js
@@ -17,34 +17,45 @@ $(document).ready(function () {
         $(".home-header-left-searchpanel-recent").css("display", "none");
     });
 
+    // Auto refresh
+    setInterval(function() {
+        console.log("refresh");
+        
+        $.ajax({
+            type: 'GET',
+            url: '/',
+            success: function () {
+                $('.home-timeline-body').load('/ .home-timeline-body');
+                // Hide all posts
+                $('.profile-post-container-status-post').css("display", "none");
+            }
+        });
+    }, 15000);
+
     // Hide all posts
     $('.profile-post-container-status-post').css("display", "none");
     showPostsInHome();
 
     // Auto refresh
-    setInterval(function() {
-        $.ajax({
-            type: 'GET',
-            url: '/',
-            success: function () {
-                location.reload();
-                // console.log("refresh");
-            }
-        })
-    }, 60000);
+    // setInterval(function() {
+    //     $.ajax({
+    //         type: 'GET',
+    //         url: '/',
+    //         success: function () {
+    //             location.reload();
+    //             // console.log("refresh");
+    //         }
+    //     })
+    // }, 60000);
 
-    // $.ajax({
-    //     type: 'GET',
-    //     url: '/',
-    //     success: function () {
-    //         setInterval('location.reload()', 60000); 
-    //     }
-    // });
+
+
+ 
    
 });
 
 function showPostsInHome() {
-    $('.profile-post-container-status-post').slice(0, 10).show();
+    $('.profile-post-container-status-post').slice(0, 10).show().slideDown();
 
     // listen for scroll event and load more images if we reach the bottom of window
     window.addEventListener('scroll', () => {
